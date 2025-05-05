4208850123f2c2d489ca137b2b7c1b63a8961d01
Jonathan_Tinti <jtintinti@gmail.com>
2025-05-05 01:41:49 -0400

    changed repo name

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,76 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import "./index.scss";
-import Loader from "react-loaders"; 
-import AnimatedLetters from "../AnimatedLetters";
-import portfolioData from '../../data/portfolio.json';  
-import dndImage from '../../assets/images/DnD.png';
-import pythonImage from '../../assets/images/pythonlogo.png';
-import saImage from '../../assets/images/SA.png';
-
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import portfolioData from '../../data/portfolio.json'
+import dndImage from '../../assets/images/DnD.png'
+import pythonImage from '../../assets/images/pythonlogo.jpg'
+import saImage from '../../assets/images/SA.png'
 
 const imageMap = {
-    "DnD.png": dndImage,
-    "pythonlogo.png": pythonImage,
-    "SA.png": saImage
-};
-
-
-const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate'); 
-    console.log(portfolioData); 
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
-
-        return () => {
-            clearTimeout(timer); 
-        }
-    }); 
-
-    const renderPortfolio = (portfolio) => {
-        return (
-            <div className="images-container">
-                {
-                    portfolio.map((port, idx) => {
-                        return (
-                        <div className="image-box" key={idx}>
-                            <img src={imageMap[port.cover.split('/').pop()]}
-                            className="portfolio-image" 
-                            alt={port.title} />
-                            <div className="content">
-                                <p className="title">{port.title}</p>
-                                <h4 className="description">{port.description}</h4>
-                                <button
-                                    className="btn"
-                                    onClick={() => window.open(port.url)}
-                                >View</button>
-                            </div>
-                        </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
-    return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
-                        idx = {15}
-                    />
-                </h1>
-                <div>
-                    {renderPortfolio(portfolioData.portfolio)}
-                </div>
-            </div>
-            <Loader type="pacman" /> 
-        </>
-    )
+  'DnD.png': dndImage,
+  'pythonlogo.jpg': pythonImage,
+  'SA.png': saImage,
 }
 
-export default Portfolio; 
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  console.log(portfolioData)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  })
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={imageMap[port.cover.split('/').pop()]}
+                className="portfolio-image"
+                alt={port.title}
+              />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Portfolio
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/JonathanTinti.github.io">
+    <BrowserRouter basename="/Personal-Webiste">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
