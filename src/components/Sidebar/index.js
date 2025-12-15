ffafa07f25b8e10193599a5f1c3d85f7a8a721eb
Jonathan_Tinti <jtintinti@gmail.com>
2025-12-15 13:19:24 -0500

    made my app mobile friendly

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,53 +7,61 @@ import {
   faHome,
   faSuitcase,
   faUser,
+  faTimes,
+  faBars,
 } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="Jonathan" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/jonathan-tinti-a188412b3/"
-        >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href="https://github.com/yyeternal">
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+import { useState } from 'react'
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <>
+      {/* MOBILE TOGGLE BUTTON */}
+      <div className="mobile-nav-toggle" onClick={() => setShowNav(!showNav)}>
+        <FontAwesomeIcon icon={showNav ? faTimes : faBars} />
+      </div>
+
+      <div className={`nav-bar ${showNav ? 'mobile-show' : ''}`}>
+        <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+          <img src={LogoS} alt="logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="Jonathan" />
+        </Link>
+
+        <nav>
+          <NavLink onClick={() => setShowNav(false)} to="/">
+            <FontAwesomeIcon icon={faHome} />
+          </NavLink>
+
+          <NavLink onClick={() => setShowNav(false)} to="/about">
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+
+          <NavLink onClick={() => setShowNav(false)} to="/portfolio">
+            <FontAwesomeIcon icon={faSuitcase} />
+          </NavLink>
+        </nav>
+
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/jonathan-tinti-a188412b3/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://github.com/yyeternal">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
 
 export default Sidebar
